ce6a8622346fc5716901108521870f71a03ee3a5
JoasGomes <jojoca.vitor777@gmail.com>
2022-08-23 08:24:12 -0300

    Página Produto

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Produto from '../views/Produto.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/produto/:id',
+    name: 'produto',
+    component: Produto,
+    props: true
   },
 ]
 
